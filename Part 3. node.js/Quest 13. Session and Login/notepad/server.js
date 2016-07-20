d320112a41c1a5dd50a8b37d62d0e465eef03ecf
thesunghwan <thesunghwan@gmail.com>
2016-07-20 22:22:41 +0900

    수정

--- Part 3. node.js/Quest 13. Session and Login/notepad/server.js
+++ Part 3. node.js/Quest 13. Session and Login/notepad/server.js
@@ -4,7 +4,6 @@ var express = require('express'),
 var bodyParser = require('body-parser');
 var fs = require("fs");
 var session = require("express-session");
-var $ = require('jQuery');
 
 var user = [{
     username: "Yhi",
@@ -40,47 +39,45 @@ app.post("/users", function(req, res) {
     var username = req.body.username,
         password = req.body.password;
 
-		for(var i=0; i<3; i++) {
-			if((username == user[i].username) && (password == user[i].password)) {
-				sess.auth_token = user[i].auth_token;
-				res.redirect('/notepad');
-				break;
-			} else {
-				res.redirect('/');
-			}
+	for(var i=0; i<3; i++) {
+		if((username == user[i].username) && (password == user[i].password)) {
+			sess.auth_token = user[i].auth_token;
+			res.redirect('/notepad');
+			return;
 		}
+	}
+	res.redirect('/');
 });
 
 app.get('/notepad', function (req, res) {
     var sess = req.session;
 
-		for(var i=0; i<3; i++) {
-			if(user[i].auth_token == sess.auth_token) {
-				res.sendFile(path.join(__dirname + '/index.html'));
-				break;
-			} else {
-				res.redirect('/');
-			}
+	for(var i=0; i<3; i++) {
+		if(user[i].auth_token == sess.auth_token) {
+			res.sendFile(path.join(__dirname + '/index.html'));
+			return;
 		}
+	}
+	res.redirect('/');
 });
 
 app.post("/note", function(req, res) {
-		var sess = req.session;
-		var filename = req.body.content;
+	var sess = req.session;
+	var filename = req.body.filename;
     var content = req.body.content;
 
-		fs.writeFile("notes/" + filename + ".txt", content, function(err) {
+	fs.writeFile("notes/" + filename + ".txt", content, function(err) {
         if(err) {
             res.setHeader('Content-Type', 'application/json');
             res.send(JSON.stringify({
                 status: "failure"
             }));
-        }
-
-        res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify({
-            status: "success"
-        }));
+        } else {
+			res.setHeader('Content-Type', 'application/json');
+	        res.send(JSON.stringify({
+	            status: "success"
+	        }));
+		}
     });
 });
 
@@ -88,16 +85,16 @@ app.get("/note", function(req, res) {
     fs.readFile("notes/" + "note.txt", "utf-8", function(err, content) {
         if(err) {
             res.setHeader('Content-Type', 'application/json');
-            res.send(JSON.stringify({
+            res.json(JSON.stringify({
                 status: "failure"
             }));
-        }
-
-        res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify({
-            status: "success",
-            content: content
-        }));
+        } else {
+			res.setHeader('Content-Type', 'application/json');
+	        res.json(JSON.stringify({
+	            status: "success",
+	            content: content
+	        }));
+		}
     });
 });
 
