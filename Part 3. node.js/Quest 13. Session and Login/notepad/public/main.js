6dea9c79388ec23242d79aed4f8e74f134c85b93
thesunghwan <thesunghwan@gmail.com>
2016-07-21 11:37:04 +0900

    코드를 건강하게 만들어주다.

--- Part 3. node.js/Quest 13. Session and Login/notepad/public/main.js
+++ Part 3. node.js/Quest 13. Session and Login/notepad/public/main.js
@@ -2,44 +2,35 @@ $.ajax({
     url: "/note",
     method: "GET",
     success: function(res) {
-      if(res.status == "success") {
-  //      for(var i = 0; res.content.length; i++) {
-          $("#filename").val(res.filename);
-          $("#notepad").val(res.content);
-  //      }
-      }
+        if(res.status == "success") {
+            res.contents.forEach(function(content, i) {
+                var container = $("<div class='notepad-container'></div>");
+                var filenameTextarea = $("<textarea class='content' rows='1' id='filename'></textarea>");
+                var contentTextarea = $("<textarea class='content' rows='10' id='notepad'></textarea>");
+                var saveButton = $("<button id='save' class='btn-save'>Save</button>");
+                var deleteButton = $("<button id='delete' class='btn-delete'>Delete</button>");
+
+                var box = $("<button class='btn-show'> note </button>");
+
+                $("body").append(container);
+
+                $(container).append(filenameTextarea);
+                $(container).append(contentTextarea);
+                $(container).append(saveButton);
+                $(container).append(deleteButton);
+                $(container).prepend(box);
+
+                filenameTextarea.val(res.filenames[i]);
+                contentTextarea.val(content);
+            });
+        }
     }
 });
 
+
+
+
 $(".btn-save").click(function(e) {
-  e.preventDefault();
-  var filename = $("#filename").val();
-  var content = $("#notepad").val();
-  $.ajax({
-      url: "/note",
-      method: "POST",
-      data: {
-          filename: filename,
-          content: content
-      },
-      success: function(response) {
-          if(response.status == "success") {
-              alert("노트가 성공적으로 저장되었습니다.");
-          } else {
-              alert("노트 저장이 실패했습니다.");
-          }
-      }
-  });
-});
-
-/* $(".btn-new").click(function(e) {
-  var note = $("<textarea class='content' rows='1' id='filename'></textarea> <textarea class='content' rows='10' id='notepad'></textarea><button id='save' class='btn-save'>Save</button><button id='delete' class='btn-delete'>Delete</button> <br> </br>");
-  var box = $("<button class = 'btn-show'> note </button>");
-
-  $("body").append(note);
-  $("body").prepend(box);
-
-  $(".btn-save").click(function(e) {
     e.preventDefault();
     var filename = $("#filename").val();
     var content = $("#notepad").val();
@@ -58,13 +49,52 @@ $(".btn-save").click(function(e) {
             }
         }
     });
-  });
+});
 
-  $(".btn-delete").click(function(e) {
-    e.preventDefault();
-    note.hide();
-    $(".btn-show").click(function(e) {
-      note.show();
-    })
-  });
-}); */
+$(".btn-new").click(function(e) {
+    var container = $("<div class='notepad-container'></div>");
+    var filenameTextarea = $("<textarea class='content' rows='1' id='filename'></textarea>");
+    var contentTextarea = $("<textarea class='content' rows='10' id='notepad'></textarea>");
+    var saveButton = $("<button id='save' class='btn-save'>Save</button>");
+    var deleteButton = $("<button id='delete' class='btn-delete'>Delete</button>");
+
+    var box = $("<button class='btn-show'> note </button>");
+
+    $("body").append(container);
+
+    $(container).append(filenameTextarea);
+    $(container).append(contentTextarea);
+    $(container).append(saveButton);
+    $(container).append(deleteButton);
+    $(container).prepend(box);
+
+    $(saveButton).click(function(e) {
+        e.preventDefault();
+        var filename = container.find("#filename").val();
+        var content = container.find("#notepad").val();
+
+        $.ajax({
+            url: "/note",
+            method: "POST",
+            data: {
+                filename: filename,
+                content: content
+            },
+            success: function(response) {
+                if(response.status == "success") {
+                    alert("노트가 성공적으로 저장되었습니다.");
+                } else {
+                    alert("노트 저장이 실패했습니다.");
+                }
+            }
+        });
+    });
+
+    $(".btn-delete").click(function(e) {
+        e.preventDefault();
+        note.hide();
+        $(".btn-show").click(function(e) {
+            note.show();
+        })
+    });
+});
--- Part 3. node.js/Quest 13. Session and Login/notepad/server.js
+++ Part 3. node.js/Quest 13. Session and Login/notepad/server.js
@@ -1,43 +1,43 @@
 var express = require('express'),
-	path = require('path'),
-	app = express();
+path = require('path'),
+app = express();
 var bodyParser = require('body-parser');
 var fs = require("fs");
 var session = require("express-session");
 
 var user = [{
-    username: "Yhi",
-    password: "123",
-    auth_token: "sfmsdlkfmklsmfklsmdlfkmsdlkf"
+	username: "Yhi",
+	password: "123",
+	auth_token: "sfmsdlkfmklsmfklsmdlfkmsdlkf"
 }, {
-    username: "Whi",
-    password: "123",
-    auth_token: "asdfasdfadfasdfasdfasdfasdf"
+	username: "Whi",
+	password: "123",
+	auth_token: "asdfasdfadfasdfasdfasdfasdf"
 }, {
-    username: "Xhi",
-    password: "123",
-    auth_token: "qwerqwerqwerqwerqwerqerasdf"
+	username: "Xhi",
+	password: "123",
+	auth_token: "qwerqwerqwerqwerqwerqerasdf"
 }];
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(session({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: false
+	secret: 'keyboard cat',
+	resave: false,
+	saveUninitialized: false
 }))
 
 app.use('/static', express.static('public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/login.html'));
+	res.sendFile(path.join(__dirname + '/login.html'));
 });
 
 app.post("/users", function(req, res) {
-    var sess = req.session;
-    var username = req.body.username,
-        password = req.body.password;
+	var sess = req.session;
+	var username = req.body.username,
+	password = req.body.password;
 
 	for(var i=0; i<3; i++) {
 		if((username == user[i].username) && (password == user[i].password)) {
@@ -50,7 +50,7 @@ app.post("/users", function(req, res) {
 });
 
 app.get('/notepad', function (req, res) {
-    var sess = req.session;
+	var sess = req.session;
 
 	for(var i=0; i<3; i++) {
 		if(user[i].auth_token == sess.auth_token) {
@@ -65,75 +65,89 @@ app.post("/note", function(req, res) {
 	var sess = req.session;
 	var filename = req.body.filename;
 	var dirname;
-  var content = req.body.content;
+	var content = req.body.content;
 
 	for(var i=0; i<3; i++) {
 		if(user[i].auth_token == sess.auth_token) {
 			dirname = "user" + i;
 			fs.writeFile("notes/" + dirname + "/" + filename + ".txt", content, function(err) {
-					if(err) {
-							res.setHeader('Content-Type', 'application/json');
-							res.send(JSON.stringify({
-									status: "failure"
-							}));
-					} else {
-							res.setHeader('Content-Type', 'application/json');
-							res.send(JSON.stringify({
-									status: "success"
-						}));
-					}
+				if(err) {
+					res.setHeader('Content-Type', 'application/json');
+					res.send(JSON.stringify({
+						status: "failure"
+					}));
+				} else {
+					res.setHeader('Content-Type', 'application/json');
+					res.send(JSON.stringify({
+						status: "success"
+					}));
+				}
 			});
 			return;
 		}
 	}
 });
 
-/* app.get("/note", function(req, res) {
-	var filename = req.body.filename;
-	var content = req.body.content;
-
-	for(var i=0; i<3; i++) {
-    if(req.session.auth_token == user[i].auth_token) {
-      var filenames = fs.readdirSync("notes/user" + i);
+app.get("/note", function(req, res) {
+	var filename = [];
+	var content = [];
+	for(var i = 0; i < 3; i++) {
+		if(req.session.auth_token == user[i].auth_token) {
+			var filenames = fs.readdirSync("notes/user" + i);
 			var dirname = "notes/user" + i;
 			for(var i=0; i < filenames.length; i++) {
-
-				filename.push(filenames, function(err, content) {
-	    		if(err) {
-          		res.setHeader('Content-Type', 'application/json');
-	        		res.json(JSON.stringify({
-	            		status: "failure"
-	        		}));
-	    		} else {
-							res.setHeader('Content-Type', 'application/json');
-	      			res.json(JSON.stringify({
-	          			status: "success",
-									filename: filename,
-	      			}));
-					}
-				});
-
-				content.push(fs.readFileSync(dirname + '/' + filenames[i], "utf-8", function(err, content) {
-	    		if(err) {
-	        		res.setHeader('Content-Type', 'application/json');
-	        		res.json(JSON.stringify({
-	            		status: "failure"
-	        		}));
-	    		} else {
-							res.setHeader('Content-Type', 'application/json');
-		    			res.json(JSON.stringify({
-	          			status: "success",
-	            		content: content
-	      			}));
-						}
-				}));
-  		}
+				filename.push(filenames[i]);
+				content.push(fs.readFileSync(dirname + '/' + filenames[i], "utf-8"));
+			}
 		}
-		break;
+		res.setHeader('Content-Type', 'application/json');
+		res.send(JSON.stringify({
+			status: "success",
+			filenames: filename,
+			contents: content
+		}));
+		return;
 	}
-}); */
+
+	res.setHeader('Content-Type', 'application/json');
+	res.send(JSON.stringify({
+		status: "failure",
+	}));
+});
 
 
 app.listen(3000, function () {
-    console.log('App listening on port 3000!');
+	console.log('App listening on port 3000!');
 });
+
+
+/*
+, function(err, content) {
+	if(err) {
+		res.setHeader('Content-Type', 'application/json');
+		res.json(JSON.stringify({
+			status: "failure"
+		}));
+	} else {
+		res.setHeader('Content-Type', 'application/json');
+		res.json(JSON.stringify({
+			status: "success",
+			content: content
+		}
+
+
+		, function(err, content) {
+			if(err) {
+				res.setHeader('Content-Type', 'application/json');
+				res.json(JSON.stringify({
+					status: "failure"
+				}));
+			} else {
+				res.setHeader('Content-Type', 'application/json');
+				res.json(JSON.stringify({
+					status: "success",
+					filename: filename,
+				}));
+			}
+		}
+*/
